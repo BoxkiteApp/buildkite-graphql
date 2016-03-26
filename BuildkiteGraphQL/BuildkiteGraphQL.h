ece7b624159dc2bea41172193176db176c622c17
Matthew Delves <matt@reformedsoftware.com>
2016-03-26 19:09:54 +1100

    [carthage] Add in a new xcode project with correct naming

---
+++ BuildkiteGraphQL/BuildkiteGraphQL.h
@@ -0,0 +1,19 @@
+//
+//  BuildkiteGraphQL.h
+//  BuildkiteGraphQL
+//
+//  Created by Matthew Delves on 26/03/2016.
+//  Copyright © 2016 BoxkiteApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BuildkiteGraphQL.
+FOUNDATION_EXPORT double BuildkiteGraphQLVersionNumber;
+
+//! Project version string for BuildkiteGraphQL.
+FOUNDATION_EXPORT const unsigned char BuildkiteGraphQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuildkiteGraphQL/PublicHeader.h>
+
+
---
+++ BuildkiteGraphQLTests/BuildkiteGraphQLTests.swift
@@ -0,0 +1,36 @@
+//
+//  BuildkiteGraphQLTests.swift
+//  BuildkiteGraphQLTests
+//
+//  Created by Matthew Delves on 26/03/2016.
+//  Copyright © 2016 BoxkiteApp. All rights reserved.
+//
+
+import XCTest
+@testable import BuildkiteGraphQL
+
+class BuildkiteGraphQLTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
